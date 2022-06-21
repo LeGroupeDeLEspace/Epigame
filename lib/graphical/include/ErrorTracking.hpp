9379a91fa0139a01f38d08c91981a2e55b84a676
Alexis Bosquillon de Jenlis <alexis.bosquillon-de-jenlis@epitech.eu>
2022-06-21 14:43:23 +0200

    changed ERR_LOCATION macro using append

--- lib/graphical/include/ErrorTracking.hpp
+++ lib/graphical/include/ErrorTracking.hpp
@@ -1,6 +1,8 @@
 #ifndef ERROR_TRACKING_HPP_
 #define ERROR_TRACKING_HPP_
 
-#define ERR_LOCATION (std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" -> ") + std::string(__FUNCTION__))
+#include <string>
+
+#define ERR_LOCATION (std::string(__FILE__).append(":").append(std::to_string(__LINE__)).append(" -> ").append(__FUNCTION__))
 
 #endif
