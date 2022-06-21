f98246ef0380a523cb8185a2a93aa6add067bfc2
Alexis Bosquillon de Jenlis <alexis.bosquillon-de-jenlis@epitech.eu>
2022-06-21 14:31:59 +0200

    macro to track error position

---
+++ lib/graphical/include/ErrorTracking.hpp
@@ -0,0 +1,6 @@
+#ifndef ERROR_TRACKING_HPP_
+#define ERROR_TRACKING_HPP_
+
+#define ERR_LOCATION (std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" -> ") + std::string(__FUNCTION__))
+
+#endif
