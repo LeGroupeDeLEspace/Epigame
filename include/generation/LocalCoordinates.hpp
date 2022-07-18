582ff97ac166b2b97acccc106c0aedb7c73a76fd
JA-RO <michel-gerald.kangmeni@epitech.eu>
2022-07-18 19:16:51 +0200

    Added Planet proper generation

---
+++ include/generation/LocalCoordinates.hpp
@@ -0,0 +1,14 @@
+//
+// Created by mgkan on 18/07/2022.
+//
+
+#ifndef JEUDELESPACE_LOCALCOORDINATES_HPP
+#define JEUDELESPACE_LOCALCOORDINATES_HPP
+
+struct Coordinates {
+    float x;
+    float y;
+    float z;
+};
+
+#endif //JEUDELESPACE_LOCALCOORDINATES_HPP
