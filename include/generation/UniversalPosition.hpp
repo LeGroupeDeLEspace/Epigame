aef3b21d507d11d4707bc7263ed9461a880c7c60
Iannis Poquet <ianpo@live.fr>
2022-07-24 16:56:27 +0200

    use i64vec3 instead of ivec3

--- include/generation/UniversalPosition.hpp
+++ include/generation/UniversalPosition.hpp
@@ -12,9 +12,9 @@ private:
 public:
     uint32_t seedUniverse;
     glm::ivec3 positionGalaxy;
-    glm::ivec3 positionSolarSystem;
+    glm::i64vec3 positionSolarSystem;
 
-    UniversalPosition(uint32_t seedUniverse, glm::ivec3 galaxy, glm::ivec3 solarSystem);
+    UniversalPosition(uint32_t seedUniverse, glm::ivec3 galaxy, glm::i64vec3 solarSystem);
     UniversalPosition(uint32_t seedUniverse, glm::ivec3 galaxy);
     UniversalPosition(uint32_t seedUniverse);
     UniversalPosition();
--- src/generation/UniversalPosition.cpp
+++ src/generation/UniversalPosition.cpp
@@ -4,7 +4,7 @@
 
 #include "generation/UniversalPosition.hpp"
 
-UniversalPosition::UniversalPosition(uint32_t seedUniverse, glm::ivec3 galaxy, glm::ivec3 solarSystem) : seedUniverse(seedUniverse), positionGalaxy(galaxy), positionSolarSystem(solarSystem) {}
+UniversalPosition::UniversalPosition(uint32_t seedUniverse, glm::ivec3 galaxy, glm::i64vec3 solarSystem) : seedUniverse(seedUniverse), positionGalaxy(galaxy), positionSolarSystem(solarSystem) {}
 UniversalPosition::UniversalPosition(uint32_t seedUniverse, glm::ivec3 galaxy) : seedUniverse(seedUniverse), positionGalaxy(galaxy), positionSolarSystem() {}
 UniversalPosition::UniversalPosition(uint32_t seedUniverse) : seedUniverse(seedUniverse), positionGalaxy(), positionSolarSystem() {}
 UniversalPosition::UniversalPosition() : seedUniverse(), positionGalaxy(), positionSolarSystem() {}
