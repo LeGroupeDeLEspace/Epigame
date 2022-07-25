29e9aad5844406d9635ef2ca1ae8c189c554eee3
Alexis Bosquillon de Jenlis <alexis.bosquillon-de-jenlis@epitech.eu>
2022-07-25 20:43:21 +0200

    index buffer

--- graphDemoScene/src/main.cpp
+++ graphDemoScene/src/main.cpp
@@ -9,28 +9,31 @@ int main()
         {{-0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
 
         {{0.0f, 0.5f}, {1.0f, 0.0f, 0.0f}},
-        {{-0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
-        {{0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
+    };
+
+    std::vector<uint16_t> index = {
+        0, 1, 2, 3, 2, 1,
     };
 
     gr::Graphics graph;
     GLFWwindow *window = gr::mainWindow.getWindow();
-    gr::Buffer &buffer = graph.newBuffer(vertices.size());
+    gr::Buffer &buffer = graph.newBuffer(vertices.size(), index.size());
     buffer.copyData(vertices.data());
+    buffer.copyIndexData(index.data());
     bool r = true;
 
     float a = 0;
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        vertices[0].pos.x = std::cos(a);
-        vertices[3].pos.x = vertices[0].pos.x;
-        a += 0.004;
-        buffer.copyData(vertices.data());
-        if (r && glfwGetKey(window, GLFW_KEY_A)) {
-            graph.removeBuffer(buffer);
-            r = false;
-        }
+        // vertices[0].pos.x = std::cos(a);
+        // vertices[3].pos.x = vertices[0].pos.x;
+        // a += 0.004;
+        // buffer.copyData(vertices.data());
+        // if (r && glfwGetKey(window, GLFW_KEY_A)) {
+        //     graph.removeBuffer(buffer);
+        //     r = false;
+        // }
         graph.test();
     }
     graph.waitForIdle();
--- lib/graphical/src/Graphics.cpp
+++ lib/graphical/src/Graphics.cpp
@@ -24,9 +24,9 @@ void Graphics::waitForIdle() {
     vkDeviceWaitIdle(this->device.getDevice());
 }
 
-Buffer &Graphics::newBuffer(size_t nvertex)
+Buffer &Graphics::newBuffer(size_t nvertex, size_t nindex)
 {
-    return this->pipeline.newBuffer(nvertex);
+    return this->pipeline.newBuffer(nvertex, nindex);
 }
 
 void Graphics::removeBuffer(Buffer &b)
