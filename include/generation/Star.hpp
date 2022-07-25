090a74c5a703852dd45f964d01be7c7827d7687b
JA-RO <michel-gerald.kangmeni@epitech.eu>
2022-07-25 10:04:18 +0200

    Added stars and moon

---
+++ include/generation/Star.hpp
@@ -0,0 +1,32 @@
+//
+// Created by jaro on 11/07/2022.
+//
+
+#ifndef JEUDELESPACE_PLANET_HPP
+#define JEUDELESPACE_PLANET_HPP
+
+#include <string>
+#include "generation/UniversalPosition.hpp"
+#include "CelestialBody.hpp"
+#include "utils/LehmerRandom.hpp"
+
+class Star : public CelestialBody {
+public:
+    enum type {                   //Based on https://en.wikipedia.org/wiki/List_of_planet_types#By_composition
+        Chthonian,
+        Gas_dwarf,
+        Gas_giant,
+        Desert,
+        Ocean,
+        Ice,
+        Rocky,
+        Metal,
+        Lava,
+        Terran
+    };
+    type bodyType;
+    LehmerRandom rand;
+    Star(std::string name, UniversalPosition position, Coordinates localPosition, float size);
+};
+
+#endif //JEUDELESPACE_PLANET_HPP
